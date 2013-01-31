e29d8332db3a383486887b8a8db97a652af84985
Francisco Souza <fss@corp.globo.com>
2013-01-31 12:03:56 -0200

    all: added license information

--- db.go
+++ db.go
@@ -1,3 +1,7 @@
+// Copyright 2013 mongoapi authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import "labix.org/v2/mgo"
