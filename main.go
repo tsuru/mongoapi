3e1c39255bcae1edb737e61f096269a5c55f09ba
Francisco Souza <fss@corp.globo.com>
2013-11-11 17:48:30 -0200

    main: add flag to print version information

--- main.go
+++ main.go
@@ -5,18 +5,37 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/bmizerany/pat"
 	"log"
 	"net/http"
 )
 
-func main() {
+const version = "0.1"
+
+var printVersion bool
+
+func init() {
+	flag.BoolVar(&printVersion, "v", false, "Print version and exit")
+	flag.Parse()
+}
+
+func buildMux() *pat.PatternServeMux {
 	m := pat.New()
 	m.Post("/resources", http.HandlerFunc(Add))
 	m.Post("/resources/:name", Handler(Bind))
 	m.Del("/resources/:name/hostname/:hostname", Handler(Unbind))
 	m.Del("/resources/:name", Handler(Remove))
 	m.Get("/resources/:name/status", Handler(Status))
+	return m
+}
+
+func main() {
+	if printVersion {
+		fmt.Printf("mongoapi version %s", version)
+		return
+	}
 	listen := coalesceEnv("0.0.0.0:3333", "MONGODB_API_LISTEN")
-	log.Fatal(http.ListenAndServe(listen, m))
+	log.Fatal(http.ListenAndServe(listen, buildMux()))
 }
